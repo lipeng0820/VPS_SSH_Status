f29a22dbeda414a2bc5d7308a3f70f6fb4479e78
lipeng0820 <lipeng0820@qq.com>
2023-10-06 12:59:05 -0500

    Update Batch_testing.sh

--- Batch_testing.sh
+++ Batch_testing.sh
@@ -4,9 +4,6 @@ echo "按下任意键立即开始执行脚本，或等待随机时间执行..."
 # 读取用户输入，超时时间设置为随机的0-180秒
 read -t $(( RANDOM % 181 )) -n 1
 
-# 获取系统当前的SSH尝试登录次数
-LOGIN_ATTEMPTS=$(grep "Failed password" /var/log/auth.log | wc -l)
-
 # 检查当前的登录模式
 if grep -q "PasswordAuthentication yes" /etc/ssh/sshd_config; then
     LOGIN_MODE="SSH"
@@ -21,6 +18,9 @@ IPV4=$(curl -s https://myip4.ipip.net || curl -s https://ddns.oray.com/checkip |
 # 获取IPv6地址
 IPV6=$(curl -s https://speed.neu6.edu.cn/getIP.php || curl -s https://v6.ident.me || curl -s https://6.ipw.cn)
 [ -z "$IPV6" ] && IPV6="无"
+
+# 获取系统当前的SSH尝试登录次数
+LOGIN_ATTEMPTS=$(grep "Failed password" /var/log/auth.log | wc -l)
 
 # 输出到日志文件
 LOG_FILENAME="VPS_ssh_status_${LOGIN_MODE}_${IPV4//./_}_${IPV6//:/_}.log"
