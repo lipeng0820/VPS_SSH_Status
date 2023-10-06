88ba71355ed5ccb0430a9e05f28432cdca7c1a63
lipeng0820 <lipeng0820@qq.com>
2023-10-06 11:56:31 -0500

    Update Batch_testing.sh

--- Batch_testing.sh
+++ Batch_testing.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# 获取本机IP地址，并将"."替换为"_"
-IP_ADDRESS=$(hostname -I | awk '{print $1}' | tr '.' '_')
+# 获取系统当前的SSH尝试登录次数
+LOGIN_ATTEMPTS=$(grep "Failed password" /var/log/auth.log | wc -l)
 
-# 检测SSH登录尝试的次数
-TOTAL_ATTEMPTS=$(grep "Failed password" /var/log/auth.log | wc -l)
-
-# 检测是否启用了SSH密码登录
+# 检查当前的登录模式
 if grep -q "PasswordAuthentication yes" /etc/ssh/sshd_config; then
     LOGIN_MODE="SSH登录"
 else
     LOGIN_MODE="私钥登录"
 fi
 
-# 生成日志文件
-LOG_FILE="/tmp/VPS_ssh_status_$IP_ADDRESS.log"
+# 获取公共IP地址
+IP_ADDR=$(curl -sSL http://httpbin.org/ip | grep -oP '"origin": "\K[^"]+')
 
-echo "SSH尝试登录次数: $TOTAL_ATTEMPTS" > $LOG_FILE
-echo "登录模式: $LOGIN_MODE" >> $LOG_FILE
+# 输出到日志文件
+echo "IP地址: $IP_ADDR" > "VPS_ssh_status_${IP_ADDR//./_}.log"
+echo "SSH尝试登录次数: $LOGIN_ATTEMPTS" >> "VPS_ssh_status_${IP_ADDR//./_}.log"
+echo "登录模式: $LOGIN_MODE" >> "VPS_ssh_status_${IP_ADDR//./_}.log"
 
-# 使用curl命令上传日志文件到网盘
-curl -k -F "file=@$LOG_FILE" -F "token=vy7v4ahy1uwt1witoqdv" -F "model=2" -F "mrid=65203924949e3" -X POST "https://tmp-cli.vx-cdn.com/app/upload_cli"
-
-# 删除临时日志文件
-rm -f $LOG_FILE
+# 使用curl上传日志文件到网盘
+curl -k -F "file=@VPS_ssh_status_${IP_ADDR//./_}.log" \
+    -F "token=vy7v4ahy1uwt1witoqdv" \
+    -F "model=2" \
+    -F "mrid=65203924949e3" \
+    -X POST "https://tmp-cli.vx-cdn.com/app/upload_cli"
